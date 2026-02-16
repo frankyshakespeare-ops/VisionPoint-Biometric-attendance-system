6d4d3d57a44e93ecffdc85a092f15ad95cfe1827
Francky_shakespeare <frankyshakespeare@gmail.com>
2026-02-16 22:42:17 +0300

    update design

--- Project101/settings.py
+++ Project101/settings.py
@@ -128,6 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGIN_URL = 'login'  # Example: 'login' if your login URL is '/login/'
